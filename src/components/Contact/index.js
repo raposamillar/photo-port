aab986ace1cbb246905ac1fff7367ecb5b54b29e
raposamillar <lisa.raposamillar@gmail.com>
2022-11-10 15:15:32 -0500

    add conditional rendering for ContactForm

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,19 +1,36 @@
 import React, { useState } from 'react';
+import { validateEmail } from '../../utils/helpers';
 
 function ContactForm() {
-
   const [formState, setFormState] = useState({ name: '', email: '', message: '' });
-
+  
+  const [errorMessage, setErrorMessage] = useState('');
   const { name, email, message } = formState;
 
   function handleSubmit(e) {
     e.preventDefault();
-    console.log(formState);
-  }
+    if (!errorMessage) {
+      setFormState({ [e.target.name]: e.target.value });
+      console.log('Form', formState);
+    }
+  };
 
   function handleChange(e) {
-    setFormState({...formState, [e.target.name]: e.target.value })
-  }
+    if (e.target.name === 'email') {
+      const isValid = validateEmail(e.target.value);
+      if (!isValid) {
+        setErrorMessage('Your email is invalid.');
+      } else {
+        setErrorMessage('');
+      } 
+    } else {
+      if (!e.target.value.length) {
+        setErrorMessage(`${e.target.name} is required.`);
+      } else {
+        setErrorMessage('');
+      }
+    }
+  };
 
   return (
   <section>
